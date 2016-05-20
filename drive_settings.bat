7e446102093139e423364a309a5fe2efcc8d4080
kenn-zx9r <kenn.zx9r@gmail.com>
2016-05-20 10:00:58 +0900

    Add files via upload
    ドライブ割り当てバッチ処理

---
+++ drive_settings.bat
@@ -0,0 +1,24 @@
+@echo off
+set STR_INPUT=Y
+
+set /P STR_INPUT="ドライブ割り当てを行います。よろしいですか？（Y/N）[Y]： "
+IF "%STR_INPUT%" == "y" (
+    goto YES
+) ELSE IF "%STR_INPUT%"=="Y" ( 
+    goto YES
+) ELSE (
+    goto FAILURE
+)
+
+:YES
+echo ドライブ割り当て中...
+net use /delete t:
+net use t: \\999.999.999.999\XXXX /user:XXX\XXXXXX XXXX
+echo ドライブ割り当てが正常に完了しました。
+pause
+exit /B 0
+
+:FAILURE
+echo 処理を中止しました。
+timeout 2
+exit /B 9
